d04f26fbb2c255d84cf7a1e11984c4fa86d6a05c
HipJaengYiCat <shee6185@gmail.com>
2024-04-23 11:39:37 +0000

    파일 비동기 처리, 파 일 읽기나JSON 파싱 과정 에러 핸들링 추가, 코드 모듈화

--- routes/result.js
+++ routes/result.js
@@ -1,21 +1,38 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs");
+const fs = require("fs"); //node.js 파일 시스템 관련 기능 사용
+
+// Import function to create page
 const createBase = require("../views/createBase.js");
 const createResult = require("../views/createResult.js");
 const { style_hrefs, srcs } = require("../utils.js");
+
 // const srcs = require("../utils.js")
 const createGraph = require("../views/createGraph.js");
 
+// Set JSON parser : JSON 형식 입력 데이터 파싱
 router.use(express.json());
 
+// Set router : 루트 경로 ('/result")에 대한 GET 요청 처리
 router.get("", function (req, res, next) {
   const type = req.query.type;
   const scores = req.query.scores;
-  const jsonData = JSON.parse(fs.readFileSync("./data/personalities.json"));
-  const result = createResult(jsonData[type]);
-  const graph = createGraph(scores);
-  res.send(createBase(type, style_hrefs, srcs, result, graph));
+
+  //readFile -> 비동기식 파일 읽기
+  fs.readFile("./data/personalities.json", (err, data) => {
+    if (err) {
+      return next(err); // 에러 핸들링
+    }
+
+    try {
+      const jsonData = JSON.parse(data);
+      const result = createResult(jsonData[type]); // 결과데이터 생성
+      const graph = createGraph(scores); // 점수 기반 그래프 생성
+      res.send(createBase(type, style_hrefs, srcs, result, graph)); // 생성페이지 클라이언트 전송
+    } catch (error) {
+      next(error); // JSON 파싱 에러 처리
+    }
+  });
 });
 
 module.exports = router;
