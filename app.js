8202f9c1a7ed68923c1f9d3e71e7e96722daae86
HipJaengYiCat <shee6185@gmail.com>
2024-04-23 11:16:28 +0000

    환경별 로깅 분리, 에러 핸들링 추가, 보안추가, 주석추가

--- app.js
+++ app.js
@@ -1,23 +1,48 @@
+// Install required modules
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const helmet = require("helmet");
+require('dotenv').config();
 
+// Import Rounter moduls
 const indexRouter = require("./routes/index");
 const resultRouter = require("./routes/result");
 const apiRouter = require("./routes/api");
 
 const app = express();
 
-app.use(logger("dev"));
+// Security middleware settings
+app.use(helmet());
+
+// Logging middleware settings
+if (process.env.NODE_ENV === "development") {
+    app.use(logger("dev"));
+} else {
+    app.use(logger("short"));
+}
+//app.use(logger("dev"));
+
+// Set up body parser and cookie parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Set path for static file service
 app.use(express.static(path.join(__dirname, "public")));
 
+// Set rounters
 app.use("/", indexRouter);
 app.use("/result", resultRouter);
 app.use("/api", apiRouter);
 
-// 서버 실행은 bin > www 파일
+// Error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+
+// Export modules, 서버 실행은 bin > www 파일
 module.exports = app;
